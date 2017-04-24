44b4bcf767fa43ae45065d64b5beeb011b5e15ae
jahnava <jahnavalmt@gmail.com>
2017-04-24 00:11:07 -0400

    Added like button

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -4,17 +4,36 @@ app.controller('MainController',
   $scope.promo = 'I\'m promoting happiness and a hopeful journey of discovery';
   $scope.products= [
         {
-          name: 'A compass to inspire a willingness to persevere through the adversities of learning angularjs. Choosing direction does\'nt come with a pricetag ',
+          name: 'A compass to inspire a willingness to persevere through the adversities of learning angularjs. Choosing direction doesn\'t come with a pricetag ',
           price: 0,
           pubdate: new Date('2017','03','23'),
-          cover: '/images/compass.gif'
+          cover: '/images/compass.gif',
+          likes: 0
         },
         {
-          name: 'love letters to god',
+          name: 'Don\'t forget to breath',
           price: 0,
           pubdate: new Date('2017', '03', '23'),
-          cover: 'images/peace.gif'
+          cover: 'images/peace.gif',
+          likes: 0
+        },
+        {
+          name: 'Don\'t forget to breath',
+          price: 0,
+          pubdate: new Date('2013', '03', '23'),
+          cover: 'images/peace.gif',
+          likes: 0
+        },
+        {
+          name: 'Don\'t forget to breath',
+          price: 0,
+          pubdate: new Date('2015', '03', '23'),
+          cover: 'images/peace.gif',
+          likes: 0
         }
         ];
-
+        // makes the likes counter go up
+        $scope.plusOne = function(index) {
+          $scope.products[index].likes += 1;
+        };
      }]);
